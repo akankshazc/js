e64c097a21f93161019d59e0b0fc17a31c724fcc
Akanksha <akankshazc23@icloud.com>
2025-03-04 23:19:08 +0530

    add examples of strict not equal operator in JavaScript

---
+++ 1_intro/17_strict_ne.js
@@ -0,0 +1,8 @@
+// same value, same type
+console.log(2 !== 2);  // false
+
+// same value, different type
+console.log(2 !== "2");  // true
+
+// different value, same type
+console.log("Hello" !== "World");  // true
