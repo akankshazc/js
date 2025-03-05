40f79e401427401c10bc4b6be6e6b372447fbdbc
Akanksha <akankshazc23@icloud.com>
2025-03-05 20:21:32 +0530

    add example of if-else statement in JavaScript

---
+++ 2_control_flow/2_if_else.js
@@ -0,0 +1,11 @@
+let age = 17;
+
+// if age is 18 or above, you are an adult
+// otherwise, you are a minor
+
+if (age >= 18) {
+    console.log("You are an adult");
+}
+else {
+    console.log("You are a minor");
+}
