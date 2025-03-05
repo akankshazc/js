83ac386110d5e1553fa2e0a44ef02061f1179e5d
Akanksha <akankshazc23@icloud.com>
2025-03-05 20:21:41 +0530

    add example of if-else statement

---
+++ 2_control_flow/3_ifelse_if.js
@@ -0,0 +1,15 @@
+let rating = 4;
+
+// rating of 2 or below is bad
+// rating of 4 or above is good
+// else, the rating is average
+
+if (rating <= 2) {
+    console.log("Bad rating");
+}
+else if (rating >= 4) {
+    console.log("Good rating!");
+}
+else {
+    console.log("Average rating");
+}
