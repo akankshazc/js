4807d68ce4604fee2b6158a7e7ed58b81a9519d3
Akanksha <akankshazc23@icloud.com>
2025-03-09 19:18:11 +0530

    add recursive factorial function example in JavaScript

---
+++ 3_functions/14_rec_factorial.js
@@ -0,0 +1,19 @@
+// recursive function
+function factorial(num) {
+
+    // base case
+    // recurse only if num is greater than 0
+    if (num > 1) {
+        return num * factorial(num - 1);
+    }
+    else {
+        return 1;
+    };
+};
+
+let x = 20;
+
+// store result of factorial() in variable
+let y = factorial(x);
+
+console.log(`The factorial of ${x} is ${y}`);
