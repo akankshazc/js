9072b41e4b13c2611a1b87035d69b85e2a06f8c4
Akanksha <akankshazc23@icloud.com>
2025-03-09 19:18:40 +0530

    add example of deleting properties from JavaScript objects

---
+++ 4_objects/5_del_prop.js
@@ -0,0 +1,11 @@
+const scientist = {
+    name: "Ratthi",
+    age: 30,
+    affiliation: "PreservationAux",
+};
+
+// delete properties
+delete scientist.affiliation
+
+// display the object
+console.log(scientist);
