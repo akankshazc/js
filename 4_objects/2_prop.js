5022e113238c5e741b10641b60ca798b1f0b59f9
Akanksha <akankshazc23@icloud.com>
2025-03-09 19:18:24 +0530

    add example of property access in JavaScript objects

---
+++ 4_objects/2_prop.js
@@ -0,0 +1,9 @@
+const person = {
+    name: "SecUnit",
+};
+
+// access property using dot notation
+console.log(person.name);
+
+// access property using bracket notation
+console.log(person["name"]);
