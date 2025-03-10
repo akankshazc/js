4dae12409500e5ebe0a45f6b4ae502bcf54f3207
Akanksha <akankshazc23@icloud.com>
2025-03-10 08:38:01 +0530

    add example of using built-in constructors in JavaScript

---
+++ 4_objects/11_builtin_constructor.js
@@ -0,0 +1,16 @@
+// use Object() constructor to create object
+const person = new Object({ name: "Mensah", age: 42 });
+
+// use String() constructor to create string object
+const name1 = new String("Mensah");
+
+// use Number() constructor to create number object
+const number = new Number(42);
+
+// use Boolean() constructor to create boolean object
+const count = new Boolean(true);
+
+console.log(person);
+console.log(name1);
+console.log(number);
+console.log(count);
