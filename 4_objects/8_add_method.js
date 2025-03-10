ef8f76588ff8dab58eaf67c9f85fbe2ea0e16472
Akanksha <akankshazc23@icloud.com>
2025-03-10 08:37:43 +0530

    add example of adding methods to JavaScript objects

---
+++ 4_objects/8_add_method.js
@@ -0,0 +1,13 @@
+// student object
+let student = {
+    name: "Amena",
+    university: "University of Mihira and Newtideland"
+};
+
+// add new method
+student.greet = function () {
+    console.log("Hello");
+};
+
+// access greet() method
+student.greet();
