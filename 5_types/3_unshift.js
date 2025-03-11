0b4fca6911ee8060801fc8d5e12509c27c92aa9f
Akanksha <akankshazc23@icloud.com>
2025-03-11 09:37:21 +0530

    add example of using unshift method to add an element to an array in JavaScript

---
+++ 5_types/3_unshift.js
@@ -0,0 +1,6 @@
+let murderbotCharacters = ["Mensah", "Rathi", "Pin-Lee"];
+
+// add an element at the beginning of the array
+murderbotCharacters.unshift("Gurathin");
+
+console.log(murderbotCharacters);
