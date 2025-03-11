f1be8230f49fcbf1b8afc83a13cea3f9fbadd082
Akanksha <akankshazc23@icloud.com>
2025-03-11 09:38:33 +0530

    add example of using push method with  a multidimensional array in JavaScript

---
+++ 5_types/9_push.js
@@ -0,0 +1,12 @@
+let studentsData = [["Jack", 24], ["Sara", 23]];
+
+// add element to the end of the outer array
+studentsData.push(["Peter", 24]);
+
+console.log(studentsData);
+
+// add "hello" as the final element
+// of the 2nd inner array
+studentsData[1].push("hello");
+
+console.log(studentsData);
