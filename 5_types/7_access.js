7543a441e9760b9b96ce80bd9da21fa86c4975f8
Akanksha <akankshazc23@icloud.com>
2025-03-11 09:37:57 +0530

    add example of accessing elements in a multidimensional array in JavaScript

---
+++ 5_types/7_access.js
@@ -0,0 +1,14 @@
+let x = [
+    ['Jack', 24],
+    ['Sara', 23],
+    ['Peter', 24]
+];
+
+// access the first item 
+console.log(x[0]);
+
+// access the first item of the first inner array
+console.log(x[0][0]);
+
+// access the second item of the third inner array
+console.log(x[2][1]);  
