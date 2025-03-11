0db4934ec7349a18f23f442cb6e2b727b1ebc44e
Akanksha <akankshazc23@icloud.com>
2025-03-11 09:37:49 +0530

    add example of creating and using a multidimensional array in JavaScript

---
+++ 5_types/6_multidim.js
@@ -0,0 +1,11 @@
+// declare three arrays
+let student1 = ['Jack', 24];
+let student2 = ['Sara', 23];
+let student3 = ['Peter', 24];
+
+// create multidimensional array
+// using student1, student2, and student3
+let studentsData = [student1, student2, student3];
+
+// print the multidimensional array
+console.log(studentsData);
