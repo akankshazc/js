6c65aa2d10269b9320f6e3425e4c41e28a25dab8
Akanksha <akankshazc23@icloud.com>
2025-03-15 22:43:43 +0530

    add example demonstrating variable hoisting with var in JavaScript

---
+++ 7_es6/2_var.js
@@ -0,0 +1,12 @@
+{
+    // block of code
+
+    // declare variable with var
+    var name = "Peter";
+
+    // can be accessed here
+    console.log(name);
+}
+
+// can be accessed here
+console.log(name);
