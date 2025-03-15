aea6708a1d16a257c005221bddd9627aefcadedd
Akanksha <akankshazc23@icloud.com>
2025-03-15 22:43:30 +0530

    add example demonstrating variable scope with let in JavaScript

---
+++ 7_es6/1_let.js
@@ -0,0 +1,12 @@
+{
+    // block of code
+
+    // declare variable with let
+    let name = "Peter";
+
+    // can be accessed here
+    console.log(name); // Peter
+}
+
+// can't be accessed here
+// console.log(name);
