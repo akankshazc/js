c06ff2ca7622fbe2c4faa5b3128e0c3a0facd099
Akanksha <akankshazc23@icloud.com>
2025-03-17 21:00:00 +0530

    add example demonstrating default parameters in a function in JavaScript

---
+++ 7_es6/10_default.js
@@ -0,0 +1,17 @@
+function sum(x = 3, y = 5) {
+    // return sum
+    return x + y;
+}
+
+// pass arguments to x and y
+var result = sum(5, 15);
+console.log(`Sum of 5 and 15: ${result}`);
+
+// pass argument to x but not to y
+result = sum(7);
+console.log(`Sum of 7 and default value (5): ${result}`);
+
+// pass no arguments
+// use default values for x and y
+result = sum();
+console.log(`Sum of default values (3 and 5): ${result}`);
