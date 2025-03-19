c85d24d9ae6abf425437ad63f3d0ad8b89302c99
Akanksha <akankshazc23@icloud.com>
2025-03-19 21:49:31 +0530

    add example demonstrating access methods in Set in JavaScript

---
+++ 7_es6/32_access.js
@@ -0,0 +1,7 @@
+const set1 = new Set([1, 2, 3]);
+
+// access the elements of a Set
+console.log(set1.values()); // Set Iterator [1, 2, 3]
+
+// check if an element is in Set
+console.log(set1.has(1)); // true
