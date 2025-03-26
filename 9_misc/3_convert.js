8d97947f07f8c59966fd9f2f5e47153f8b338875
Akanksha <akankshazc23@icloud.com>
2025-03-26 21:17:39 +0530

    add example demonstrating conversion of a JavaScript object to JSON

---
+++ 9_misc/3_convert.js
@@ -0,0 +1,8 @@
+// JavaScript object
+const jsonData = { "name": "John", "age": 22 };
+
+// converting to JSON
+const obj = JSON.stringify(jsonData);
+
+// accessing the data
+console.log(obj); // "{"name":"John","age":22}"
