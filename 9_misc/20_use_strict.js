bf52dafac8926534177532617d7869f6052c5405
Akanksha <akankshazc23@icloud.com>
2025-03-28 20:47:47 +0530

    add example demonstrating the use of 'strict mode' in JavaScript

---
+++ 9_misc/20_use_strict.js
@@ -0,0 +1,12 @@
+myVariable = 9;
+console.log(myVariable); // 9
+
+function hello() {
+
+    // applicable only for this function
+    'use strict';
+
+    string = 'hello'; // throws an error
+}
+
+hello();
